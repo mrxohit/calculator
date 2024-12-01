32fa716e4579f64a08ed6feaf07ef0a895075a00
mrxohit <xohit.email@gmail.com>
2024-12-01 11:26:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 let string = "";
 let buttons = document.querySelectorAll(".button")
-Array.from(buttons).forEach((button) => {
 
-    button.addEventListener('click', (e) => {
-        if (e.target.innerHTML == '=') {
-            string = eval(string)
-            document.querySelector('input').value = string;
-        } else if (e.target.innerHTML == 'c') {
-            string = ""
-            document.querySelector('input').value = string;
-        } else {
-            console.log(e.target)
-            string = string + e.target.innerHTML;
-            document.querySelector('input').value = string;
-        }
-    })
-});
+Array.from(buttons).forEach((button) => {
+    button.addEventListener(
+        'click', (e) => {
+            switch (e.target.innerHTML) {
+                case 'c':
+                    string = string.slice(0, string.length - 1);
+                    document.querySelector('input').value = string;
+                    break;
+                case 'ac':
+                    string = ""
+                    document.querySelector('input').value = string;
+                    break;
+                case '=':
+                    string = eval(string)
+                    document.querySelector('input').value = string;
+                    break;
+                default:
+                    string = string + e.target.innerHTML;
+                    document.querySelector('input').value = string;
+                    break;
+            }
+        })
+})
